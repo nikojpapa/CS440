e2552cd00e4de38044a48db96a01282e9b8488de
Nicholas Papadopoulos <nikojpapa@wireless1x-155-41-78-240.bu.edu>
2015-04-24 11:33:58 -0400

    p4 first working file

--- P4/p4/npapaPlayer.py
+++ P4/p4/npapaPlayer.py
@@ -2,9 +2,12 @@ import sys
 import re
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
+inf = float("inf")
+negInf = float("-inf")
+
 # print to stderr for debugging purposes
 # remove all debugging statements before submitting your code
-msg = "Given board " + sys.argv[1] + "\n"
+msg = "Given board " + sys.argv[1] + "\n\n"
 sys.stderr.write(msg)
 
 #parse the input string, i.e., argv[1]
@@ -12,9 +15,12 @@ inp = sys.argv[1].replace("[", "").split("]")
 lastPlay = inp.pop(len(inp)-1).replace("LastPlay:", "")
 remove = re.compile('(LastPlay:|\(|\))')
 lastPlay = remove.sub("", lastPlay).split(",")
-for num in lastPlay:
-	num = int(num)
-size = len(inp)
+if lastPlay[0] == "null":
+	lastPlay =  "first move"
+else:
+	for ind, num in enumerate(lastPlay):
+		lastPlay[ind] = int(num)
+size = len(inp) - 2
 board = []
 for row in reversed(inp):
 	thisRow = []
@@ -30,29 +36,44 @@ def getPoss(board, lastPlay):
 	leftPos = size+2 - rightPos - upPos
 	possTop = upPos+1 if upPos+1 <= size else upPos
 	possBottom = upPos-1 if upPos-1 >= 1 else upPos
+	pp.pprint(rightPos+1 <= size)
 	possRight = rightPos+1 if rightPos+1 <= size else rightPos
 	possLeft = rightPos-1 if rightPos-1 >= 1 else rightPos
 
 	return (upPos, rightPos, leftPos, possTop, possBottom, possRight, possLeft)
 
 
-def score(board, lastPlay):
+def scoreThis(board, lastPlay):
 	possible = getPoss(board, lastPlay)
+	pp.pprint("")
+	pp.pprint(board)
+	pp.pprint(lastPlay)
+	pp.pprint(possible)
 	possTop = possible[3]
 	possBottom = possible[4]
 	possRight = possible[5]
 	possLeft = possible[6]
 
 	numAvail = 0
-	for i in possTop..possBottom:
-		for j in possRight..possLeft:
-			if board[i][j] != "0"
-				numAvail += 1
+	for i in range(possBottom, possTop+1):
+		for j in range(possLeft, possRight+1):
+			if j < len(board[i]):
+				if board[i][j] != 0:
+					numAvail += 1
 
-	return 6 - numAvail
+	return (6 - numAvail, lastPlay)
+
+def copyBoard(curentBoard):
+	newBoard = []
+	for i in curentBoard:
+		thisCol = []
+		for j in i:
+			thisCol.append(j)
+		newBoard.append(thisCol)
+	return newBoard
 
 
-def alphaBeta(board, lastPlay, depth, turn, alpha, beta):
+def alphaBeta(board, lastPlay, depth, isMax, alpha, beta):
 	possible = getPoss(board, lastPlay)
 	upPos = possible[0]
 	rightPos = possible[1]
@@ -63,17 +84,58 @@ def alphaBeta(board, lastPlay, depth, turn, alpha, beta):
 	possLeft = possible[6]
 
 	if depth == 0:  #or node = terminal node
-		return score(board, lastPlay)
+		return scoreThis(board, lastPlay)
 	else:
-		if turn is "max":
-			score = float("-inf")
+		if isMax:
+			score = (negInf, [])
 
-			for i in possTop..possBottom:  #for each child
-				for j in possRight..possLeft:
+			for i in range(possBottom, possTop+1):  #for each child
+				for j in range(possLeft, possRight+1):
 					if board[i][j] != 0:
 						continue #(skip if move not available)
-					child = 
-					childScore = score(board, lastPlay)
+					child = copyBoard(board)
+					for color in range(1, 3):  #for each color
+						child[i][j] = color
+						childScore = alphaBeta(child, [color, i, j, size-i-j], depth-1, False, alpha, beta)
+						if childScore[0] > score[0]:
+							score = childScore
+						if score[0] > alpha:
+							alpha = score[0]
+						if beta <= alpha:
+							break
+					if beta <= alpha:
+						break
+				if beta <= alpha:
+					break
+			return score
+		else:
+			score = (inf, [])
+
+			for i in range(possBottom, possTop+1):  #for each child
+				for j in range(possLeft, possRight+1):
+					if board[i][j] != 0:
+						continue #(skip if move not available)
+					child = copyBoard(board)
+					for color in range(1, 3):  #for each color
+						child[i][j] = color
+						childScore = alphaBeta(child, [color, i, j, size-i-j], depth-1, True, alpha, beta)
+						if childScore[0] < score[0]:
+							score = childScore
+						if score[0] < beta:
+							beta = score
+						if beta <= alpha:
+							break
+					if beta <= alpha:
+						break
+				if beta <= alpha:
+					break
+			return score
+
+
+pp.pprint(copyBoard(board))
+pp.pprint(getPoss(board, lastPlay))
+pp.pprint(scoreThis(board, lastPlay))
+pp.pprint(alphaBeta(board, lastPlay, 1, True, negInf, inf))
 
 
 				
